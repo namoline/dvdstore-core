10c388804e7a14a49d42e9303e2c146c4c2ceac4
Nancy Nemouthe <mirman_759@sfr.fr>
2024-06-28 22:17:04 +0200

    traitement exercice 6

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,6 +3,8 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.repository.FileMovieRepository;
 import com.mycompany.dvdstore.service.DefaultMovieService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -12,14 +14,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
-        //System.out.println( "Hello World!" );
-        FileMovieRepository movieRepository= new FileMovieRepository();
-        DefaultMovieService movieService= new DefaultMovieService();
-
-        movieService.setMovieRepository(movieRepository);
-        MovieController movieController = new MovieController();
-        movieController.setMovieService(movieService);
+        ApplicationContext context= new ClassPathXmlApplicationContext("applicationContext.xml");
+        MovieController movieController=context.getBean(MovieController.class);
 
         movieController.addUsingConsole();
     }
