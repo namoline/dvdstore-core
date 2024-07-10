aa016d5f202199282a8532522f9358154535bb23
Nancy Nemouthe <mirman_759@sfr.fr>
2024-07-10 17:24:55 +0200

    traitement exercice 20

--- src/main/java/com/mycompany/dvdstore/controller/AboutUsController.java
+++ src/main/java/com/mycompany/dvdstore/controller/AboutUsController.java
@@ -9,6 +9,6 @@ public class AboutUsController {
     @RequestMapping("/about-us")
     public String displayFullDetails(){
       System.out.println("Tentative d'affichage de l'a-propos");
-        return "";
+        return "about-us";
     }
 }
